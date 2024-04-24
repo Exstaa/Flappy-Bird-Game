3142fce62714c4a6064273ef5749a9d1d33655b5
Exstaa <exstagithub@gmail.com>
2024-04-24 18:15:43 +0400

    Main class is created.

---
+++ main/Main.java
@@ -0,0 +1,24 @@
+package az.hafizrzazade.flappybird.main;
+
+import javax.swing.JFrame;
+
+import az.hafizrzazade.flappybird.game.FlappyBird;
+
+public class Main {
+	   public static void main(String[] args) {
+		   int boardWidth = 360;
+		   int boardHeight = 640;
+		   
+		   JFrame frame = new JFrame("Flappy Bird");
+		   frame.setSize(boardWidth, boardHeight);
+		   frame.setLocationRelativeTo(null);
+		   frame.setResizable(false);
+		   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		   
+		   FlappyBird flappyBird = new FlappyBird();
+		   frame.add(flappyBird);
+		   frame.pack();
+		   flappyBird.requestFocus();
+		   frame.setVisible(true);
+	   }
+}
